14af4459c8193de5bde1912227a959b6014e4685
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-07-28 22:27:47 +0200

    chore(image-viewer-2): update App

--- project-2-react-image-viewer-2/src/App.jsx
+++ project-2-react-image-viewer-2/src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+import ImgContainer from './components/ImgContainer.jsx';
+import ImgNotSelected from './components/imgNotSelected.jsx';
+import { products as data } from "./data/products.js"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeProduct, setActiveProduct] = useState(data.find(item => item.isActive));
+  const [products, setProducts] = useState(data);
 
   return (
     <>
-      hello
+      <main className="container">
+        {
+          activeProduct ? <img src={activeProduct.imgName} style={{ width: '100%' }} /> : <ImgNotSelected />
+        }
+        <ImgContainer products={products} />
+      </main>
     </>
   )
 }
