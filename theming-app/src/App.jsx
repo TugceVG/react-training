577600a2d0195b6fa6cdd4956546dfdb5430d89c
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-08-30 18:30:37 +0200

    feat(theming-app) : add routing and update components

--- theming-app/src/App.jsx
+++ theming-app/src/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <h1>App</h1>
+      <Navbar />
+      <div className="container my-5">
+        <div className="row mt-3">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+      <Footer />
     </>
   )
 }
--- theming-app/src/pages/Home.jsx
+++ theming-app/src/pages/Home.jsx
@@ -3,7 +3,87 @@ import React from 'react'
 export default function Home() {
     return (
         <>
-            Home
+            <div className="col-sm-12">
+                <h1>Home</h1>
+                <hr />
+                {/* article*4>h2>{Article $}^p*4>lorem20^ul>li*10>lorem4 */}
+                <article>
+                    <h2>Article 1</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, sunt nisi veniam repellat omnis blanditiis culpa vel fugiat. At, quidem.</p>
+                    <p>Eum eligendi quaerat fuga nemo sequi beatae sapiente eveniet autem iusto obcaecati nihil, unde hic nobis assumenda est corrupti! Neque!</p>
+                    <p>Eaque similique, voluptates omnis officia quo ratione explicabo, laudantium exercitationem reiciendis sit perspiciatis libero culpa provident error quas fugiat ipsam?</p>
+                    <p>Optio distinctio odio quidem repellat animi eos aliquam eum ipsam suscipit numquam veniam sed labore inventore odit, cumque non. Quia.</p>
+                    <ul>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Dicta ipsam quis quibusdam.</li>
+                        <li>Mollitia officiis repellendus autem!</li>
+                        <li>Quibusdam dolores omnis iste!</li>
+                        <li>Fuga deserunt assumenda iste?</li>
+                        <li>Iure eius ab hic.</li>
+                        <li>Tempore neque illum ut.</li>
+                        <li>Sunt maxime tempore et.</li>
+                        <li>Deleniti totam quaerat fuga.</li>
+                        <li>Esse facere porro nostrum.</li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Article 2</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium veniam placeat harum consequatur sed beatae excepturi repellendus, esse hic iure.</p>
+                    <p>Reiciendis maxime quia laboriosam iure nisi! Dolorum sint, iusto impedit iste suscipit consequuntur molestiae officia porro cupiditate. Iusto, fuga assumenda!</p>
+                    <p>Ratione reiciendis suscipit culpa laboriosam placeat doloribus cupiditate distinctio, accusamus quidem ut omnis provident ea sit illo hic neque architecto.</p>
+                    <p>Provident sequi possimus vitae! Ex ducimus, aliquam possimus quo alias est eum animi rem asperiores deserunt tenetur consequuntur architecto aspernatur.</p>
+                    <ul>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Incidunt porro ducimus maiores.</li>
+                        <li>Necessitatibus fugit esse voluptatibus.</li>
+                        <li>Repellat dolorum explicabo tenetur!</li>
+                        <li>Architecto ut assumenda obcaecati?</li>
+                        <li>Maiores porro impedit molestiae!</li>
+                        <li>Quas, eos. Magni, vero.</li>
+                        <li>Facere explicabo ipsa aut!</li>
+                        <li>Ducimus unde magni corporis.</li>
+                        <li>Iusto consectetur aperiam eius.</li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Article 3</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus nobis veniam facilis repellendus vel. Autem perspiciatis dolor odio asperiores aliquam?</p>
+                    <p>Soluta, quae quidem magnam minima quis alias, vel perspiciatis nemo at error rem. Molestias, saepe at a magnam unde porro.</p>
+                    <p>At asperiores ad quis et neque libero culpa nulla veniam numquam labore! Delectus reprehenderit, aperiam obcaecati placeat consequuntur cumque aut!</p>
+                    <p>Provident maxime et animi facilis tenetur maiores quia nam, eveniet tempora minima sapiente inventore unde magni hic minus vitae enim.</p>
+                    <ul>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Molestias quidem quae quas!</li>
+                        <li>Iure ut magnam facere.</li>
+                        <li>Reiciendis alias quidem dolor.</li>
+                        <li>Amet voluptates eveniet ut!</li>
+                        <li>Vero repellat quia molestias.</li>
+                        <li>Odio nostrum corrupti debitis!</li>
+                        <li>Ad placeat itaque laudantium.</li>
+                        <li>Earum eos aut alias.</li>
+                        <li>Deserunt aut optio obcaecati.</li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Article 4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ex ipsam natus minus rerum aperiam atque ut, dolores expedita maxime.</p>
+                    <p>Animi quidem temporibus maxime, quos ipsum, beatae cumque velit repellat rerum ea perferendis aut fugit corporis delectus molestias quaerat assumenda.</p>
+                    <p>Omnis consequuntur soluta quos consequatur placeat quisquam obcaecati vero nostrum quaerat laudantium eos beatae sequi voluptatem itaque quo, cum possimus.</p>
+                    <p>Eaque at facere corporis laboriosam corrupti, quis debitis repudiandae minima eum, maiores, neque iure. Sint recusandae eos quidem soluta asperiores?</p>
+                    <ul>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Quibusdam recusandae quidem non.</li>
+                        <li>Placeat itaque nostrum laboriosam?</li>
+                        <li>Ipsum dolore sequi illum?</li>
+                        <li>Esse officia nemo totam.</li>
+                        <li>Perferendis aspernatur debitis eum.</li>
+                        <li>Officiis quod possimus quam!</li>
+                        <li>Quam asperiores nisi cupiditate?</li>
+                        <li>Alias expedita pariatur soluta!</li>
+                        <li>Expedita eveniet commodi quam?</li>
+                    </ul>
+                </article>
+            </div>
         </>
     )
 }
