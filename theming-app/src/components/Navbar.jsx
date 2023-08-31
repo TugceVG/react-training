e21c584d16af233796f2533e6f2774b25e8e6a21
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-08-31 12:03:18 +0200

    feat(theming-app) : add simple theme without context API

--- theming-app/src/components/Navbar.jsx
+++ theming-app/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import ThemeIcon from './ThemeIcon'
 
 export default function Navbar() {
     return (
@@ -10,10 +11,12 @@ export default function Navbar() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
+                    <div className="navbar-nav ms-auto">
                         <NavLink className="nav-link" to="/">Home</NavLink>
                         <NavLink className="nav-link" to="/about">About</NavLink>
                         <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                        <ThemeIcon />
+
                     </div>
                 </div>
             </div>
