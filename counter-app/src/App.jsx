37969e13c6c32af6923f3fb29c52beb3d41b6edf
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-09-07 22:02:13 +0200

    chore(counter-app) : counter with useState

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -3,10 +3,26 @@ import { useState } from 'react';
 function App() {
   const [count, setCount] = useState(0)
 
+  function increment() {
+    setCount(prev => prev + 1)
+  }
+
+  function decriment() {
+    setCount(prev => prev - 1)
+  }
+
+  function reset() {
+    setCount(0)
+  }
+
   return (
     <>
+      <h2>Count :: {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decriment}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </>
   )
 }
 
-export default App
+export default App;
